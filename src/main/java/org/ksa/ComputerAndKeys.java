e7b8caffc8c31625aaa7ea817f4711908475f86e
LucyFerrr <akieranscott99@gmail.com>
2025-11-13 22:15:11 +0800

    Initial commit

---
+++ src/main/java/org/ksa/ComputerAndKeys.java
@@ -0,0 +1,11 @@
+package org.ksa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ComputerAndKeys {
+    public static void main(String[] args) {
+        SpringApplication.run(ComputerAndKeys.class, args);
+    }
+}
